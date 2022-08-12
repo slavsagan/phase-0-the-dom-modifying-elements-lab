7664520b1d36d2d04a0e40a4977bb28b8fd6f448
Slav <itiksolution@gmail.com>
2022-08-12 19:12:04 +1000

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute('id', 'victory')
+
+newHeader.innerHTML = 'Slav is the champion'
+newHeader.textContent = 'Slav is the champion'
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,5 @@
 describe("index.html", () => {
+  
   describe("after index.js is processed", () => {
     it("no longer has DOM node 'main#main'", () => {
       expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
